c1b887716292a00a6c82514c5bdce5c3b8ac21dd
vladryanka <vsmorzhok@mail.ru>
2025-03-27 23:11:46 +0300

    Initial commit

---
+++ app/src/main/java/com/smorzhok/coroutineflow/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.smorzhok.coroutineflow
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.smorzhok.coroutineflow.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.buttonUsersActivity.setOnClickListener {
+            startActivity(UsersActivity.newIntent(this))
+        }
+    }
+}
---
+++ app/src/main/java/com/smorzhok/coroutineflow/UsersRepository.kt
@@ -0,0 +1,22 @@
+package com.smorzhok.coroutineflow
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+object UsersRepository {
+
+    private val users = mutableListOf("Nick", "John", "Max")
+
+    suspend fun addUser(user: String) {
+        delay(10)
+        users.add(user)
+    }
+
+    suspend fun loadUsers(): Flow<List<String>> = flow {
+        while (true) {
+            emit(users.toList())
+            delay(500)
+        }
+    }
+}
