1b6994687c03e3c490926d7d826824270ad3a869
vladryanka <vsmorzhok@mail.ru>
2025-03-28 17:25:10 +0300

    Операторы ЖЦ Flow

---
+++ app/src/main/java/com/smorzhok/coroutineflow/lesson4/CryptoRepository.kt
@@ -0,0 +1,38 @@
+package com.smorzhok.coroutineflow.lesson4
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlin.random.Random
+
+object CryptoRepository {
+
+    private val currencyNames = listOf("BTC", "ETH", "USDT", "BNB", "USDC")
+    private val currencyList = mutableListOf<Currency>()
+
+    fun getCurrencyList(): Flow<List<Currency>> = flow {
+        while (true){
+            delay(3000)
+            generateCurrencyList()
+            emit(currencyList.toList())
+            delay(3000)
+        }
+    }
+
+    private fun generateCurrencyList() {
+        val prices = buildList {
+            repeat(currencyNames.size) {
+                add(Random.nextInt(1000, 2000))
+            }
+        }
+        val newData = buildList {
+            for ((index, currencyName) in currencyNames.withIndex()) {
+                val price = prices[index]
+                val currency = Currency(name = currencyName, price = price)
+                add(currency)
+            }
+        }
+        currencyList.clear()
+        currencyList.addAll(newData)
+    }
+}
---
+++ app/src/main/java/com/smorzhok/coroutineflow/lesson4/Currency.kt
@@ -0,0 +1,6 @@
+package com.smorzhok.coroutineflow.lesson4
+
+data class Currency(
+    val name: String,
+    val price: Int
+)
